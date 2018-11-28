99a638d2da29c347b16b256f3f7786f515f4c756
Matthew Strimaitis <matthewstrimaitis@MacBook-Pro.local>
2018-11-27 20:24:16 -0600

    Commit for version with animals, some scripts and animation

---
+++ Assets/GameObjectScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectScript : MonoBehaviour {
+	
+	BoxCollider coll;
+	GameObject hand;
+	public float speed = 1;
+	public Vector3 target;
+
+	// Use this for initialization
+	void Start () {
+		coll = GetComponent<BoxCollider> ();
+		hand = GameObject.Find ("Hand");
+		target = new Vector3 (hand.transform.position.x, hand.transform.position.y, hand.transform.position.z);
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		if (true) {
+			// transform.position = Vector3.MoveTowards (transform.position, target, speed * Time.deltaTime);
+			transform.parent = other.transform;
+			transform.Translate (Vector3.up * 40 * Time.deltaTime);
+			hand.transform.position = Vector3.MoveTowards (hand.transform.position, transform.position, speed * Time.deltaTime);
+			coll.isTrigger = false;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ Assets/bearscript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bearscript : MonoBehaviour {
+
+	GameObject terrain;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.forward * Time.deltaTime);
+
+		// RaycastHit hit;
+		// Physics.Raycast;
+
+		int layerMask = 1 << 8;
+		layerMask = ~layerMask;
+
+		RaycastHit hit;
+		// Does the ray intersect any objects excluding the player layer
+		if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+		{
+			Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+			Debug.Log("Did Hit");
+		}
+		else
+		{
+			Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+			Debug.Log("Did not Hit");
+		}
+	}
+
+	/*
+	void onCollisionEnter(Collision other) {
+		float heightatpoint = terraindata.GetHeight ((int)transform.position.x, (int)transform.position.z);
+		transform.position = new Vector3(transform.position.x, heightatpoint + 1.0f, transform.position.z);
+	}
+	*/
+}
